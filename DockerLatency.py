57f683056b4d39128b5d29677945f4c85e7c49c7
Mihai Gramada <gramada.mihai@gmail.com>
2015-10-12 16:50:08 +0200

    fixed docker latency

--- DockerLatency.py
+++ DockerLatency.py
@@ -1,21 +1,34 @@
+from docker import Client
+import os
+from AbstractLatency import AbstractLatency
+import utils
+
 class DockerLatency(AbstractLatency):
     def __init__(self, base_url):
         self.client = Client(base_url=base_url)
 
     def add_latency(self, node_name, target_interface, mean, dev):
-        self.client.exec_create(node_name, utils.latency_command.format(target_interface,mean,dev))
+        res = self.client.exec_create(node_name, utils.latency_command.format(target_interface,mean,dev))
+        self.client.exec_start(res['Id'])
 
     def add_duplication(self, node_name, target_interface, percentage_duplicate):
-        self.client.exec_create(node_name, utils.duplication_command.format(target_interface, percentage_duplicate))
+        res = self.client.exec_create(node_name, utils.duplication_command.format(target_interface, percentage_duplicate))
+        self.client.exec_start(res['Id'])
 
 
     def add_reorder(self, node_name, target_interface, delay_time, percentage_packets, correlation_percentage ):
         # see documentation in utils.py for explanation of these parameters
-        self.client.exec_create(node_name, utils.reorder_command.format(target_interface, delay_time, percentage_packets, correlation_percentage))
+        res = self.client.exec_create(node_name, utils.reorder_command.format(target_interface, delay_time, percentage_packets, correlation_percentage))
+        self.client.exec_start(res['Id'])
 
     def add_loss(self, node_name, target_interface, percentage_loss):
-        self.client.exec_create(node_name, utils.packet_loss_command.format(target_interface, percentage_loss))
+        res = self.client.exec_create(node_name, utils.packet_loss_command.format(target_interface, percentage_loss))
+        self.client.exec_start(res['Id'])
 
-    def add_corruption(self, node_name, percentage_corrupt):
-        self.client.exec_create(node_name, utils.corruption_command.format(target_interface, percentage_corrupt))
+    def add_corruption(self, node_name, target_interface, percentage_corrupt):
+        res = self.client.exec_create(node_name, utils.corruption_command.format(target_interface, percentage_corrupt))
+        self.client.exec_start(res['Id'])
+    def reset(self, node_name, target_interface):
+        res = self.client.exec_create(node_name, utils.reset_command.format(target_interface))
+        self.client.exec_start(res['Id'])
 
--- basic.py
+++ basic.py
@@ -1,9 +1,13 @@
 import random
+import subprocess
 import time
 import utils
 
+from DockerLatency import DockerLatency
+
 VAGRANT_FILE = "/Users/mga/Documents/thesis/ers-utils"
 target_interface = "eth1"
+docker_path = '/usr/bin/docker'
 
 
 def active_node_names_list(vagrant_instance):
@@ -19,8 +23,42 @@ def toggle_with_delay(vagrant_instance, node_name, sleep_duration):
     time.sleep(sleep_duration)
     turn_on_node(v, node_name)
 
+def running_containers_list():
+    running_containers_command = (docker_path + ' ps -q').split(' ')
+
+    result = subprocess.check_output(running_containers_command)
+    containers_list = []
+    for container_id in result.split():
+		if container_id[-1] == '\n':
+			container_id = container_id[:-1]
+		containers_list.append(container_id)
+    return containers_list
+
 
 def main():
+	node_list = running_containers_list()
+	dl = DockerLatency("unix://var/run/docker.sock")
+	for node in node_list:
+		#dl.add_latency(node, 'eth1', 100, 0)
+		#dl.add_latency(node, 'eth0', 100, 0)
+                #dl.add_loss(node, 'eth0', 15)
+                #dl.add_loss(node, 'eth1', 15)
+                #dl.add_duplication(node, 'eth0', 60)
+                #dl.add_duplication(node, 'eth1', 60)
+                #dl.add_corruption(node, 'eth0', 15)
+                #dl.add_corruption(node, 'eth1', 15)
+                dl.add_reorder(node, 'eth0', 100, 25, 50)
+                dl.add_reorder(node, 'eth1', 100, 25, 50)
+	import pdb;pdb.set_trace()
+
+
+        
+        for node in node_list:
+            dl.reset(node, 'eth0')
+            dl.reset(node, 'eth1')
+
+
+def main_vagrant():
     v = vagrant.Vagrant(VAGRANT_FILE)
 
     nodes = active_node_names_list(v)
